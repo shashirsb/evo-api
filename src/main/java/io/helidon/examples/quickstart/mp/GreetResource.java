b388964036e5556db924b84b3532782191e6ce51
shashirsb <shasrama@SHASRAMA-IN.in.oracle.com>
2020-11-10 14:33:16 +0530

    adding

--- src/main/java/io/helidon/examples/quickstart/mp/GreetResource.java
+++ src/main/java/io/helidon/examples/quickstart/mp/GreetResource.java
@@ -183,7 +183,7 @@ public class GreetResource {
     }
 
     private JsonObject findUser(String userid) {
-        if(userid.toStrin() == "shashi") {
+        if(userid.toString() == "shashi") {
         return JSON.createObjectBuilder()
                     .add( "userid", userid)
                     .add( "firstname", "samuel")
