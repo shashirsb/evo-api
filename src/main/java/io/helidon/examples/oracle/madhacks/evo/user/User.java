50c625f7de5b30b18402fb6efb3fbbef992b84c3
shashirsb <shasrama@SHASRAMA-IN.in.oracle.com>
2020-11-11 12:10:52 +0530

    adding java

---
+++ src/main/java/io/helidon/examples/oracle/madhacks/evo/user/User.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright (c) 2020 Oracle and/or its affiliates.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at
+ * http://oss.oracle.com/licenses/upl.
+ */
+
+package io.helidon.examples.oracle.madhacks.evo;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Priority;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Alternative;
+import javax.inject.Inject;
+
+
+
+
+import org.eclipse.microprofile.opentracing.Traced;
+import static javax.interceptor.Interceptor.Priority.APPLICATION;
+
+///////////////////
+
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Specializes;
+import javax.inject.Inject;
+
+import org.bson.BsonDocument;
+import org.bson.conversions.Bson;
+import org.eclipse.microprofile.opentracing.Traced;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonArray;
+
+import java.nio.file. * ;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import io.helidon.config.Config;
+import io.helidon.webserver.Routing;
+import io.helidon.webserver.ServerRequest;
+import io.helidon.webserver.ServerResponse;
+import io.helidon.webserver.Service;
+
+import java.io. * ;
+import java.util.Properties;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.HashMap;
+import java.util.stream.Stream;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+import oracle.soda.rdbms.OracleRDBMSClient;
+import oracle.soda.OracleDatabase;
+import oracle.soda.OracleCursor;
+import oracle.soda.OracleCollection;
+import oracle.soda.OracleDocument;
+import oracle.soda.OracleException;
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.ParseException;
+import org.json.simple.parser.JSONParser;
+
+import org.apache.commons.lang3.StringUtils;
+import java.time.LocalDateTime;
+
+import com.google.gson.Gson;
+import io.helidon.examples.oracle.madhacks.evo. * ;
+
+/**
+ * An implementation of
+ * {@link io.helidon.examples.sockshop.carts.CartRepository} that that uses
+ * MongoDB as a backend data store.
+ */
+
+@ApplicationScoped@Alternative@Priority(APPLICATION)@Traced
+public class User  {
+
+	public static EvoProvider asp = new EvoProvider();
+	public static OracleDatabase db = asp.dbConnect();
+
+	@Inject
+	void AtpSodaCartRepository() {
+		try {
+			String UserResponse = createData();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	
+	public String createData() {
+
+		try {
+			OracleCollection col = this.db.admin().createCollection("users");
+			col.admin().truncate();
+		} catch(OracleException e) {
+			e.printStackTrace();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return "successfully created users collection !!!";
+	}
+
+}
