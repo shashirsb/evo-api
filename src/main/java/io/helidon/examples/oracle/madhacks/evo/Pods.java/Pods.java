0362a3b732ffd69d95586bbb02ca4c5ef02b6dd8
shashirsb <shasrama@SHASRAMA-IN.in.oracle.com>
2020-11-19 10:24:40 +0530

    adding

--- src/main/java/io/helidon/examples/oracle/madhacks/evo/Pods.java/Pods.java
+++ src/main/java/io/helidon/examples/oracle/madhacks/evo/Pods.java/Pods.java
@@ -114,7 +114,7 @@ public class Pods  {
             resultDoc = null;
             String jsonFormattedString = null;
 
-            OracleDocument filterSpec = db.createDocumentFromString("{ \"$or\" : [ {\"evopod\": {\"amperage\": {\"$not\" : {\"$eq\" : \"\"}}}},{\"evopod\": {\"$not\" : {\"$eq\" : {}}}]}");
+            OracleDocument filterSpec = db.createDocumentFromString("{ \"$or\" : [ {\"evopod\": {\"amperage\": {\"$not\" : {\"$eq\" : \"\"}}}},{\"evopod\": {\"$not\" : {\"$eq\" : {}}}}]}");
             System.out.println("filterSpec: -------" + filterSpec.getContentAsString());
 
             resultDoc = col.find().filter(filterSpec).getOne();
