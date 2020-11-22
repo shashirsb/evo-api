9c603cd8721060bcdd2c3c44dfe9bcd68681a024
shashi_bhushan_ramachandra <shashi.bhushan.ramachandra@oracle.com>
2020-11-22 12:14:44 +0530

    adding

--- src/main/resources/add_external_jars.sh
+++ src/main/resources/add_external_jars.sh
@@ -1,5 +1,5 @@
 #!/bin/sh 
 
-SOCKSHOP_CATALOG_ATPSODA=$HOME/madhacks/helidon/code/evo-api
+EVO_ATPSODA=$HOME/madhacks/helidon/code/evo-api
 
-for i in ojdbc8 ucp osdt_core osdt_cert oraclepki javax.json-1.0.4 json-20200518 json-simple-1.1 nio_char orajsoda-1.1.4 org.apache.commons.io  xmlparserv2; do  mvn install:install-file -Dfile=$SOCKSHOP_CATALOG_ATPSODA/src/main/resources/libs/$i.jar -DgroupId=com.oracle.jdbc -DartifactId=$i -Dversion=19.3.0 -Dpackaging=jar  -DgeneratePom=true; done;
+for i in ojdbc8 ucp osdt_core osdt_cert oraclepki javax.json-1.0.4 json-20200518 json-simple-1.1 nio_char orajsoda-1.1.4 org.apache.commons.io  xmlparserv2; do  mvn install:install-file -Dfile=$EVO_ATPSODA/src/main/resources/libs/$i.jar -DgroupId=com.oracle.jdbc -DartifactId=$i -Dversion=19.3.0 -Dpackaging=jar  -DgeneratePom=true; done;
